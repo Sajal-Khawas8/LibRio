dc67bba9c013f24999715460c9293183b713f60d
SAJAL KHAWAS <khawassajal756@gmail.com>
2026-05-27 12:15:37 +0530

    Config changes

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,11 +2,11 @@
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
+echo "Running migrations..."
+php artisan migrate --seed --force
+
 echo "Clearing cache..."
 php artisan optimize:clear
 
 echo "Caching..."
 php artisan optimize:cache
-
-echo "Running migrations..."
-php artisan migrate --seed --force
